16c3fc853c4a3a4689a533ed1c13cb4725856c65
younessbennaj <youness.bennaj@gmail.com>
2024-12-09 17:56:55 +0700

    feat: add masonry layout article

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -155,8 +155,8 @@ function DesktopNavigation(props) {
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
+        {/* <NavItem href="/projects">Projects</NavItem>
+        <NavItem href="/speaking">Speaking</NavItem> */}
         <NavItem href="/uses">Uses</NavItem>
       </ul>
     </nav>
@@ -409,10 +409,10 @@ export function Header() {
                   </AvatarContainer>
                 )}
               </div>
-              {/* <div className="flex flex-1 justify-end md:justify-center">
-                <MobileNavigation className="pointer-events-auto md:hidden" />
+              <div className="flex flex-1 justify-end md:justify-center">
+                {/* <MobileNavigation className="pointer-events-auto md:hidden" /> */}
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
-              </div> */}
+              </div>
               <div className="flex justify-end md:flex-1">
                 <div className="pointer-events-auto">
                   <ThemeToggle />
