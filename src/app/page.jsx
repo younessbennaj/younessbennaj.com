ea985e2756f67a49048efce07992e37177ef7f0c
younessbennaj <youness.bennaj@gmail.com>
2025-10-13 22:48:56 +0900

    refactor: update grid layout for improved responsiveness on larger screens

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -92,7 +92,7 @@ export default async function Home() {
             <div className="block md:hidden">
               <Image src={hero} alt="Youness Bennaj" />
             </div>
-            <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2 lg:gap-y-12">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-16 md:grid-cols-2 md:gap-y-12">
               <div className="pt-8">
                 <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
                   Développeur web & Consultant en expatriation au Japon
