a3509c471141b1eda60eb7cfda6ccec7d0f70ef1
younessbennaj <youness.bennaj@gmail.com>
2025-11-10 18:07:47 +0900

    feat: add articles on expatriation to Japan with structured content and metadata

---
+++ src/app/articles/guide-expatriation-japon-developpeur/meta.js
@@ -0,0 +1,26 @@
+import banner from '@/images/banner-2.jpg'
+
+export const article = {
+  author: 'Youness Bennaj',
+  createdAt: '2025-06-26',
+  category: 'expatriation',
+  image: banner,
+  tags: [
+    {
+      label: 'Japon',
+      value: 'japon',
+    },
+    {
+      label: 'Développement',
+      value: 'developpement',
+    },
+    {
+      label: 'Expatriation',
+      value: 'expatriation',
+    },
+  ],
+  title:
+    "Guide complet pour s'expatrier et travailler au Japon en tant que développeur",
+  description:
+    "Une vue d'ensemble structurée des étapes pour s'installer et lancer sa carrière au Japon en tant que développeur ou talent tech.",
+}
---
+++ src/app/articles/pourquoi-vivre-au-japon/meta.js
@@ -0,0 +1,11 @@
+import banner from '@/images/banner-3.jpg'
+
+export const article = {
+  author: 'Youness Bennaj',
+  createdAt: '2025-07-30',
+  category: 'expatriation',
+  image: banner,
+  title: "Pourquoi j'ai choisi de partir vivre au Japon",
+  description:
+    "Dans cet article, je partage les raisons personnelles et professionnelles qui m'ont poussé à faire le grand saut vers le Japon, et ce que cela signifie de s’expatrier dans un pays aussi fascinant que complexe.",
+}
